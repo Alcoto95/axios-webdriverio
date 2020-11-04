211f865f6c0ce354ce4ccd179e8f35487ed3bace
Alcoto95 <actorruco@gmail.com>
2020-11-04 13:34:12 -0600

    Add login negative scenarios

--- Functional/tests/login_tests.js
+++ Functional/tests/login_tests.js
@@ -3,9 +3,27 @@ import homePage from '../page_objects/home_page.js'
 import * as data from '../../Utils/constants.js'
 
 describe('Todoist Login Test Suite', () => {
-    it('Login - Happy Path', () => {
+    it('TC-0001 - Login - Empty Email and Password', () => {
+        loginPage.open();
+        loginPage.loginFlow();
+        expect(loginPage.errorMessage).toHaveText('Invalid email address.');
+    });
+
+    it('TC-0002 - Login - Invalid Email', () => {
+        loginPage.open();
+        loginPage.loginFlow('wrongemail@gmail.com', data.TODOIST_PASWORD);
+        expect(loginPage.errorMessage).toHaveText('Wrong email or password.');
+    });
+
+    it('TC-0003 - Login - Invalid Password', () => {
+        loginPage.open();
+        loginPage.loginFlow(data.TODOIST_EMAIL, 'invalidPassword');
+        expect(loginPage.errorMessage).toHaveText('Wrong email or password.');
+    });
+
+    it('TC-0004 - Login - Happy Path', () => {
         loginPage.open();
         loginPage.loginFlow(data.TODOIST_EMAIL, data.TODOIST_PASWORD);
         expect(homePage.todayHeader).toHaveText('Today');
-    })
+    });
 })
